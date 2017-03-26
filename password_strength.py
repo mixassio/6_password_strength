b47fb44e53b3364ab3920fc2881a1d9f5c3ce062
mixassio <mixail.bondarev@gmail.com>
2017-03-27 00:17:25 +0300

    add code on server for change in work

--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,74 @@
+import re
 
 
 def get_password_strength(password):
-    pass
+    status_pasw = 1
+    # check popular pasword
+    popular_pasw = ['123456', '123456789', 'qwerty', '111111', '1234567', '666666', '12345678', '7777777', '123321',
+                    '0', '654321', '1234567890', '123123', '555555', 'vkontakte', 'gfhjkm', '159753', '777777',
+                    'TempPassWord', 'qazwsx', '1q2w3e', '1234', '112233', '121212', 'qwertyuiop', 'qq18ww899',
+                    '987654321', '12345', 'zxcvbn', 'zxcvbnm', '999999', 'samsung', 'ghbdtn', '1q2w3e4r', '1111111',
+                    '123654', '159357', '131313', 'qazwsxedc', '123qwe', '222222', 'asdfgh', '333333', '9379992',
+                    'asdfghjkl', '4815162342', '12344321', '88888888', '11111111', 'knopka', '789456', 'qwertyu',
+                    '1q2w3e4r5t', 'iloveyou', 'vfhbyf', 'marina', 'password', 'qweasdzxc', '10203', '987654', 'yfnfif',
+                    'cjkysirj', 'nikita', '888888', 'vfrcbv', 'k.,jdm', 'qwertyuiop[]', 'qwe123', 'qweasd', 'natasha',
+                    '123123123', 'fylhtq', 'q1w2e3', 'stalker', '1111111111', 'q1w2e3r4', 'nastya', '147258369',
+                    '147258', 'fyfcnfcbz', '1234554321', '1qaz2wsx', 'andrey', '111222', '147852', 'genius', 'sergey',
+                    '7654321', '232323', '123789', 'fktrcfylh', 'spartak', 'admin', 'test', '123', 'azerty', 'abc123',
+                    'lol123', 'easytocrack1', 'hello', 'saravn', 'holysh!t', '1', 'Test123', 'tundra_cool2', '456',
+                    'dragon', 'thomas', 'killer', 'root', '1111', 'pass', 'master', 'aaaaaa', 'a', 'monkey', 'daniel',
+                    'asdasd', 'e10adc3949ba59abbe56e057f20f883e', 'changeme', 'computer', 'jessica', 'letmein',
+                    'mirage', 'loulou', 'lol', 'superman', 'shadow', 'admin123', 'secret', 'administrator', 'sophie',
+                    'kikugalanetroot', 'doudou', 'liverpool', 'hallo', 'sunshine', 'charlie', 'parola', '100827092',
+                    '/', 'michael', 'andrew', 'password1', 'fuckyou', 'matrix', 'cjmasterinf', 'internet', 'hallo123',
+                    'eminem', 'demo', 'gewinner', 'pokemon', 'abcd1234', 'guest', 'ngockhoa', 'martin', 'sandra',
+                    'asdf', 'hejsan', 'george', 'qweqwe', 'lollipop', 'lovers', 'q1q1q1', 'tecktonik', 'naruto', '12',
+                    'password12', 'password123', 'password1234', 'password12345', 'password123456', 'password1234567',
+                    'password12345678', 'password123456789', '000000', 'maximius', '123abc', 'baseball1', 'football1',
+                    'soccer', 'princess', 'slipknot', '11111', 'nokia', 'super', 'star', '666999', '12341234',
+                    '1234321', '135790', '159951', '212121', 'zzzzzz', '121314', '134679', '142536', '19921992',
+                    '753951', '7007', '1111114', '124578', '19951995', '258456', 'qwaszx', 'zaqwsx', '55555', '77777',
+                    '54321', 'qwert', '22222', '33333', '99999', '88888', '66666']
+    if password in popular_pasw:
+        return status_pasw
+    else:
+        status_pasw += 1
+        print('not in list+1')
+    #check character
+    if re.findall('([a-zA-Z])', password) != []:
+        status_pasw += 1
+        print('character+1')
+    #check on upper and lower case
+    is_upp = set()
+    is_low = set()
+    for letter in password:
+        is_low.add(letter.islower())
+        is_upp.add(letter.isupper())
+    if True in is_low and True in is_upp:
+        status_pasw += 2
+        print('lower+upper+2')
+    #check number
+    if re.findall('(\d+)', password) != []:
+        status_pasw += 1
+        print('number+1')
+    #check %^&*
+    if re.findall('[^\w\s]', password) != []:
+        status_pasw += 1
+        print('punctuation+1')
+    if len(password) > 6:
+        status_pasw += 1
+        print('lenght+1')
+    #check include year
+    status_pasw += 1
+    print('year+1')
+    for year in range(1950,2017):
+        if password.find(str(year)) > 0:
+            status_pasw -= 1
+            print('year-1')
+            break
+    return status_pasw
+
 
 
 if __name__ == '__main__':
-    pass
+    print(get_password_strength('hj197UI%erty'))
